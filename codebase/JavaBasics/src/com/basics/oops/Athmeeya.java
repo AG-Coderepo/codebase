c9926a4d95130c1852f78450bfb5f657a95ca64f
N R, Athmeeya <Athmeeya.NR@emc.com>
2021-05-02 08:11:38 +0530

    First Commit

---
+++ codebase/JavaBasics/src/com/basics/oops/Athmeeya.java
@@ -0,0 +1,42 @@
+package com.basics.oops;
+
+public class Athmeeya {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Good Morning");
+
+	}
+
+}
+
+
+/*
+ * Types of Java Platforms
+ * 
+ * 1.Java SE - OOPS,Multi-threading,Strings(For Selenium dev)
+ * 
+ * 2.Java EE - JSP ,EJP (For Normal Browser Dev)
+ * 
+ * 3.Java ME(Micro Edition) - Development of Mobile application
+ * 
+ * 4.Java FX - Swing (To Develop Rich Internet Application(76% of applicarion is devleoped using this technology)
+ *              
+ * Other Technologies used of developing rich internet appications:
+ * Adobe Flash - It can even be used to develop Games
+ * Microsoft Silverlight
+ * 
+ * JAVA is OOP Language and Concetps of OOPS are:
+ * 1.Object(State/Data and behaviour)
+ * 2.Class
+ * 3.Inheritance
+ * 4.Ploymorphism
+ * 5.Abstraction
+ * 6.Encpasulation
+ * 
+ * 
+ * Java better than C++:
+ * 1.Removed rarely used features such as explicit pointers ,operator overloading
+ * 2.Automatic Garbage collection
+ * 3.Easy to learn after C++
+ * **/
