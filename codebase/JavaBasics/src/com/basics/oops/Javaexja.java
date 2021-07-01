3bd99c5693970c356083405f2a2539d50aefa177
N R, Athmeeya <Athmeeya.NR@emc.com>
2021-07-02 01:15:25 +0530

    Changed the label

--- codebase/JavaBasics/src/com/basics/oops/Javaexja.java
+++ codebase/JavaBasics/src/com/basics/oops/Javaexja.java
@@ -4,12 +4,13 @@ import javax.swing.*;
 public class Javaexja {
 	
 	
-        public static void main(String[] args) {
+        public static void main(String[] args) 
+        {
             //Create and set up the window.
             JFrame frame = new JFrame("HelloWorldSwing");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            JLabel label = new JLabel("Hello World");
+            JLabel label = new JLabel("Hello Athmeeya");
             frame.add(label);
 
             //Display the window.
