c7e4bfc8c1cd3c03a46fc8de1aa2b90bef373273
N R, Athmeeya <Athmeeya.NR@emc.com>
2021-07-02 01:03:17 +0530

    New class files added

---
+++ codebase/JavaBasics/src/com/basics/oops/Javaexja.java
@@ -0,0 +1,24 @@
+package com.basics.oops;
+import javax.swing.*;
+
+public class Javaexja {
+	
+	
+        public static void main(String[] args) {
+            //Create and set up the window.
+            JFrame frame = new JFrame("HelloWorldSwing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JLabel label = new JLabel("Hello World");
+            frame.add(label);
+
+            //Display the window.
+            frame.pack();
+            frame.setVisible(true);
+        
+    }
+    class Dummy {
+        // just to have another thing to pack in the jar
+    }
+
+}
---
+++ codebase/JavaBasics/src/com/basics/oops/Primitivedatatpe.java
@@ -0,0 +1,24 @@
+package com.basics.oops;
+
+public class Primitivedatatpe {
+
+	public static void main(String[] args) {
+
+		//There are eight primtive data types in Java
+		
+        byte  a = 123;
+        short b = 1234;
+        int   c = 12345;
+        long  d = 123456;
+        float e = 1.2f;
+        double f = 12.345;
+        boolean g = true;
+        char h = 'k';
+     
+        System.out.println("Eigth Primitive Data Types of Java are -"+"byte : "+a+" ,short : "+b+" ,int : "+c+" ,long : "+d+" \n"
+        		+" ,float : "+e+" ,double : "+f+" ,boolean : "+g+" ,char : "+h);
+        
+	   System.out.println("Non-Primitve Data Types of Java are Strings ,Arrays and Classes");
+	}
+
+}
