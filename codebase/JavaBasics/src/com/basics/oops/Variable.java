1e40bbbc3c0f00e2bcfd1f07fcb7a4a281e541fb
N R, Athmeeya <Athmeeya.NR@emc.com>
2021-07-02 17:33:04 +0530

    adding all files

---
+++ codebase/JavaBasics/src/com/basics/oops/Variable.java
@@ -0,0 +1,15 @@
+package com.basics.oops;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		//Variable Declaration
+		int a ;
+		//Variable Initialization
+		a = 10;
+		//Variable Utilitzation
+		System.out.println("Value present inisde a is : "+a);
+
+	}
+
+}
