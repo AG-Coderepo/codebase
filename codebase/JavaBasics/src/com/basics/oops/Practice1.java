ed71ac10f8d5b01cf6344593cf08d1172cdab7f1
NR, Athmeeya <Athmeeya.NR@emc.com>
2025-04-12 17:35:17 +0530

    Create Practice1.java
    Commit

---
+++ codebase/JavaBasics/src/com/basics/oops/Practice1.java
@@ -0,0 +1,15 @@
+package com.basics.oops;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(2);
+		System.out.println(12.34);
+		System.out.println("Hello Athmeeya");
+		System.out.println(true);
+		System.out.println('A');
+
+	}
+
+}
