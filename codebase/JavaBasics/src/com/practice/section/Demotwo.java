d96e00d4f4cb95ca4cc6549489bc56209ddeeeae
NR, Athmeeya <Athmeeya.NR@emc.com>
2025-04-14 15:54:09 +0530

    Commit
    protected access modifier

---
+++ codebase/JavaBasics/src/com/practice/section/Demotwo.java
@@ -0,0 +1,12 @@
+package com.practice.section;
+import com.practice.section.Protectmethodofclassdemo;
+//extends Protectmethodofclassdemo
+public class Demotwo  extends Protectmethodofclassdemo{
+
+	public static void main(String[] args) {
+		Demotwo obj = new Demotwo();
+		obj.methodOne();
+
+	}
+
+}
---
+++ codebase/JavaBasics/src/com/practice/section/Protectmethodofclassdemo.java
@@ -0,0 +1,11 @@
+package com.practice.section;
+
+public class Protectmethodofclassdemo {
+
+	int k = 10;
+	protected void methodOne()
+	{
+		System.out.println("Content of the protected method");
+	}
+	
+}
