d419287e23e6bd96f6bb230480cd7aebc43750f6
NR, Athmeeya <Athmeeya.NR@emc.com>
2025-04-15 20:10:18 +0530

    Create FindPrimenum.java
    commit

---
+++ codebase/JavaBasics/src/com/practice/section/FindPrimenum.java
@@ -0,0 +1,17 @@
+package com.practice.section;
+
+public class FindPrimenum {
+
+	public static void main(String[] args) {
+		int n = 3;
+		
+		if(n % 2 == 0)
+		{
+			System.out.println(n+" : is not a prime number");
+		}else
+		{
+			System.out.println(n+" : is prime number");
+		}
+	}
+
+}
